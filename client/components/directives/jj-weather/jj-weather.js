7075fcae17e015b902c3d93596f7952107ad2c68
Juan <jsamesub@gmail.com>
2014-09-17 01:06:21 -0500

    jj-weather

---
+++ client/components/directives/jj-weather/jj-weather.js
@@ -0,0 +1,32 @@
+/* jshint camelcase:false */
+(function(){
+  'use strict';
+
+  angular.module('jjWeatherModule',[])
+  .factory('WeatherApi',['$http', function($http){
+    function conditions(zip){
+    return $http.jsonp('http://api.wunderground.com/api/f0e6fdcbf9d5f727/conditions/q/' + zip + '.json?callback=JSON_CALLBACK');
+    }
+
+    return {conditions:conditions};
+  }])
+
+  .directive('jjWeather', [function(){
+    var o = {};
+
+    o.restrict = 'A';                                                                                 // herre is to tell how it can be use A = atribute,C = class,D,M = comment
+    o.templateUrl = '/components/directives/jj-weather/jj-weather.html';
+    o.scope       = {zip:'@'};                                                                                      // true create his how schope and hereda from his parent - false the used there own + there one from his parents. {} = Is asolated and only work with his own scope
+    o.link        =   function(scope, element, attrs){                         // Link is a function that take 3 parameters ( $scope, element= is th div, atributte ) Is like havin a controller in the div
+                      };
+
+    o.controller  =  ['$scope', 'WeatherApi', function($scope, WeatherApi){
+                        WeatherApi.conditions($scope.zip).then(function(response){
+                          debugger;
+                          $scope.temperature = response.data.current_observation.temp_f;
+                          $scope.icon = response.data.current_observation.icon_url;
+                      });
+                    }];
+    return o;
+  }]);
+})();
