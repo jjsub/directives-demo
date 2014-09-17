80520161a4a9a00bd3f1c7856d17393508808e97
Juan <jsamesub@gmail.com>
2014-09-16 21:14:39 -0500

    false, true, {}

---
+++ client/components/directives/jj-greeting/jj-greeting.js
@@ -0,0 +1,16 @@
+(function(){
+  'use strict';
+
+  angular.module('jjGreetingModule',[])
+  .directive('jjGreeting', [function(){
+    var o = {};
+
+    o.restrict = 'A'; // herre is to tell how it can be use A = atribute,C = class,D,M = comment
+    o.templateUrl = '/components/directives/jj-greeting/jj-greeting.html';
+    o.scope = false, true, {}   // true create his how schope and hereda from his parent - false the used there own + there one from his parents. {} = Is asolated and only work with his own scope
+
+    return o;
+
+    };
+  }]);
+})();
